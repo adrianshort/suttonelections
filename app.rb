e6eee3b89d91303f5aad6450bc5112f528e9188e
Adrian Short <adrian.short@gmail.com>
2010-04-13 14:19:09 +0100

    Text additions. Moved to pat gem. Added Datamapper code

--- app.rb
+++ app.rb
@@ -1,25 +1,49 @@
 require 'rubygems'
 require 'sinatra'
-require 'json'
-require 'rest_client'
+require 'pat'
+require 'dm-core'
+require 'dm-validations'
+require 'dm-timestamps'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/db.sqlite3")
+
+class Postcode
+  include DataMapper::Resource
+
+  property :id,             Integer, :serial => true
+  property :postcode,       String,  :required => true
+  property :created_at,     DateTime
+  property :lat,            Float
+  property :lng,            Float
+  property :district_name,  String
+  property :district_code,  String
+  property :ward_name,      String
+  property :ward_code,      String
+end
+
+DataMapper.auto_upgrade!
 
 get '/' do
-  @wards = %w{ Cheam Sutton Stonecot }
   haml :home
 end
 
 get '/wards' do
   @postcode = params[:postcode].strip.upcase
-
-  url = "http://www.uk-postcodes.com/postcode/" + @postcode.gsub(/ /, '') + '.json'
-
-  result = RestClient.get(url)
-  result_ary = JSON.parse(result)
-  @district_name = result_ary['administrative']['district']['title']
-  @ward_name = result_ary['administrative']['ward']['title']
+  result = Pat.get(@postcode)
+  @district_name = result['administrative']['district']['title']
+  @ward_name = result['administrative']['ward']['title']
   haml :wards
 end
 
+get '/how-the-election-works' do
+  haml :election
+end
+
+# get '/voting' do
+#   haml :voting
+# end
+
 get '/about' do
+  @accounts = %w{ adrianshort mashthestate openlylocal openelection lbsuttonnews stef stonecothill sutmoblib mysociety pezholio stonecotparking }
   haml :about
 end
