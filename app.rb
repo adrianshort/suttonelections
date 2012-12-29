97d77da9cce4c002f740ca475655593529591f9a
Adrian Short <adrian.short@gmail.com>
2012-12-29 16:45:26 +0000

    Sort candidate's candidacies in date order. Fix #7
    This needs to be written as an SQL query as there's no way I can see in
    DataMapper to order a result set by a column in a joined table (the
    election date).

--- app.rb
+++ app.rb
@@ -159,6 +159,37 @@ end
 
 get '/candidates/:id/?' do
   if @candidate = Candidate.get(params[:id])
+    @candidacies = repository(:default).adapter.select("
+      SELECT
+        e.d,
+        c.*,
+        p.name AS party_name,
+        p.colour AS party_colour,
+        b.name AS body_name,
+        b.slug AS body_slug,
+        b.districts_name AS districts_name,
+        d.name AS district_name,
+        d.slug AS district_slug
+      
+      FROM candidacies c
+      
+      INNER JOIN elections e
+      ON c.election_id = e.id
+      
+      INNER JOIN parties p
+      ON c.party_id = p.id
+      
+      INNER JOIN bodies b
+      ON e.body_id = b.id
+      
+      INNER JOIN districts d
+      ON c.district_id = d.id
+      
+      WHERE c.candidate_id = #{@candidate.id}
+      
+      ORDER BY d
+    ")
+  
     haml :candidate
   else
     404
